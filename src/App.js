626360fbe0abcab5e6a03a0c1d43e1a43521f0cd
Vishesh <vishesh.thanki@gmail.com>
2022-01-18 11:45:55 -0500

    Add initial components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+
+const links = [
+  "https://i.imgur.com/5He8kDp.jpeg",
+  "https://i.imgur.com/tBuw3vZ.jpeg",
+  "https://i.imgur.com/1IQPIOO.jpeg",
+  "https://i.imgur.com/EkfoAfa.jpeg",
+  "https://i.imgur.com/edjM6Ae.jpeg",
+  "https://i.imgur.com/NfGIpaO.jpeg",
+  "https://i.imgur.com/cLK6Rly.jpeg",
+  "https://i.imgur.com/JOgNZOe.jpeg",
+  "https://i.imgur.com/KPD9x09.jpeg",
+  "https://i.imgur.com/pw8HddL.jpeg",
+  "https://i.imgur.com/dorXvvZ.jpeg",
+  "https://i.imgur.com/4ZCemHm.jpeg",
+  "https://i.imgur.com/mVH5UxJ.jpeg",
+  "https://i.imgur.com/6CVy5iF.jpeg",
+  "https://i.imgur.com/Gb3fdKq.jpeg",
+  "https://i.imgur.com/gD52hF4.jpeg",
+  "https://i.imgur.com/5wY4fjI.jpeg",
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
