f316873c20cdaf8aa5637b4a3b4779fc7b683d60
Vishesh <vishesh.thanki@gmail.com>
2022-01-18 16:42:06 -0500

    Update styling, Add Home/Listings components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import "./App.scss";
-
-const links = [
-  "https://i.imgur.com/5He8kDp.jpeg",
-  "https://i.imgur.com/tBuw3vZ.jpeg",
-  "https://i.imgur.com/1IQPIOO.jpeg",
-  "https://i.imgur.com/EkfoAfa.jpeg",
-  "https://i.imgur.com/edjM6Ae.jpeg",
-  "https://i.imgur.com/NfGIpaO.jpeg",
-  "https://i.imgur.com/cLK6Rly.jpeg",
-  "https://i.imgur.com/JOgNZOe.jpeg",
-  "https://i.imgur.com/KPD9x09.jpeg",
-  "https://i.imgur.com/pw8HddL.jpeg",
-  "https://i.imgur.com/dorXvvZ.jpeg",
-  "https://i.imgur.com/4ZCemHm.jpeg",
-  "https://i.imgur.com/mVH5UxJ.jpeg",
-  "https://i.imgur.com/6CVy5iF.jpeg",
-  "https://i.imgur.com/Gb3fdKq.jpeg",
-  "https://i.imgur.com/gD52hF4.jpeg",
-  "https://i.imgur.com/5wY4fjI.jpeg",
-];
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Listings from "./Listings";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div style={{ margin: "20px" }}>
+      <BrowserRouter>
+        <nav style={{ marginBottom: "20px" }}>
+          <span style={{ marginRight: "50px" }}>
+            <Link to="/">Home</Link>
+          </span>
+          <span>
+            <Link to="/show">Listings</Link>
+          </span>
+        </nav>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="show" element={<Listings />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Listings.js
@@ -0,0 +1,32 @@
+export default function Listings() {
+  const links = [
+    "https://i.imgur.com/5He8kDp.jpeg",
+    "https://i.imgur.com/tBuw3vZ.jpeg",
+    "https://i.imgur.com/1IQPIOO.jpeg",
+    "https://i.imgur.com/EkfoAfa.jpeg",
+    "https://i.imgur.com/edjM6Ae.jpeg",
+    "https://i.imgur.com/NfGIpaO.jpeg",
+    "https://i.imgur.com/cLK6Rly.jpeg",
+    "https://i.imgur.com/JOgNZOe.jpeg",
+    "https://i.imgur.com/KPD9x09.jpeg",
+    "https://i.imgur.com/pw8HddL.jpeg",
+    "https://i.imgur.com/dorXvvZ.jpeg",
+    "https://i.imgur.com/4ZCemHm.jpeg",
+    "https://i.imgur.com/mVH5UxJ.jpeg",
+    "https://i.imgur.com/6CVy5iF.jpeg",
+    "https://i.imgur.com/Gb3fdKq.jpeg",
+    "https://i.imgur.com/gD52hF4.jpeg",
+    "https://i.imgur.com/5wY4fjI.jpeg",
+  ];
+
+  return (
+    <>
+      <h1>Listings</h1>
+      <div>
+        {links.map((link, index) => {
+          return <img src={link} key={index} />;
+        })}
+      </div>
+    </>
+  );
+}
